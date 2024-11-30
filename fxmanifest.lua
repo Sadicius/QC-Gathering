30fb81a4072fd8338aba739f7eb1fa40c2974454
Artmines <96462463+Artmines@users.noreply.github.com>
2024-11-30 16:33:26 -0700

    Version Check added

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,11 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+quantum_github 'https://github.com/Quantum-Projects-RedM/QC-Gathering'
 author 'Quantum Projects'
 description 'Quantum Gathering.'
 
-version '0.0.1'
+version '1.0.0'
 
 shared_script {
 	'@ox_lib/init.lua',
